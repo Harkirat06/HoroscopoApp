450ac4fa45ff8f42745857a69921eac557c944f3
moha11 <mohamed.tanger2001@gmail.com>
2024-04-22 15:18:19 +0200

    Comentadas las funciones toDomain()

--- app/src/main/java/dadm/hsingh/horoscopoapp/data/horoscope/model/HoroscopeDtoMapper.kt
+++ app/src/main/java/dadm/hsingh/horoscopoapp/data/horoscope/model/HoroscopeDtoMapper.kt
@@ -6,7 +6,7 @@ import java.io.IOException
 fun Response<DailyHoroscopeDto>.toDomain() =
     if (isSuccessful) Result.success((body() as DailyHoroscopeDto).toDomain())
     else Result.failure(IOException())
-
+/*
 fun Response<WeeklyHoroscopeDto>.toDomain() =
     if (isSuccessful) Result.success((body() as WeeklyHoroscopeDto).toDomain())
     else Result.failure(IOException())
@@ -14,3 +14,5 @@ fun Response<WeeklyHoroscopeDto>.toDomain() =
 fun Response<MonthlyHoroscopeDto>.toDomain() =
     if (isSuccessful) Result.success((body() as MonthlyHoroscopeDto).toDomain())
     else Result.failure(IOException())
+
+ */
