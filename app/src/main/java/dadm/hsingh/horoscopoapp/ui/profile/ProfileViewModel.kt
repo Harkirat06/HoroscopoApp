5ceff85b8404e8ac48cac67665529bc1e542e2b0
Alberto S. <asansan5@upv.edu.es>
2024-05-12 13:48:27 +0200

    profile personal info hecho: no sé que pasa con las imágenes del compatibily, pero ahora no me van bien

--- app/src/main/java/dadm/hsingh/horoscopoapp/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/dadm/hsingh/horoscopoapp/ui/profile/ProfileViewModel.kt
@@ -10,9 +10,11 @@ import dadm.hsingh.horoscopoapp.domain.model.ZodiacSign
 import dadm.hsingh.horoscopoapp.domain.model.ZodiacSigns.ZodiacSigns
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -22,8 +24,10 @@ class ProfileViewModel @Inject() constructor(
     private val rep: FriendsRepository,
     private val settingsRepository: SettingsRepository
 ) : ViewModel() {
+
     private val zodiacSigns = ZodiacSigns()
-    private val profile_sign = rep.getFriendById("USUARIO")
+    val profile_sign = rep.getFriendById("USUARIO")
+
 
     private val _language : MutableStateFlow<String> = MutableStateFlow("")
     val language get() = _language.asStateFlow()
